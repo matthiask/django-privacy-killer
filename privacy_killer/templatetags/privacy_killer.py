7062881ffccea19dff4b7a6e76a88b1b1cf3107a
albanbochsler <alb@feinheit.ch>
2019-10-18 17:01:59 +0200

    Black

--- privacy_killer/templatetags/privacy_killer.py
+++ privacy_killer/templatetags/privacy_killer.py
@@ -16,7 +16,6 @@ SNIPPETS = [
         "privacy_killer/google_tag_manager_body.html",
     ),
     (r"^UA-", "privacy_killer/google_analytics_head.html", None),
-
     # FBQ- is only used as identificator and does not appear inside markup
     (r"^FBQ-", "privacy_killer/facebook_pixel_head.html", None),
 ]
